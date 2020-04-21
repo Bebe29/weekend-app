6383a56eb07c6ae8e6dfb6df921c5dd65acfb50a
Bebe29 <bevinda.je@gmail.com>
2020-04-21 09:42:27 +0700

    day 10

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import reducers from "./views/redux/reducers";
+import reducers from "./redux/reducers";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
